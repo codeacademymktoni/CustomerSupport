f26284386026f9aea3cdf1c7ebff6c4b850085a7
tnakevski@outlook.com <tnakevski@outlook.com>
2020-07-04 10:51:29 +0200

    initial create

---
+++ Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSupport.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public DateTime DateCreated { get; set; }
+        [Required]
+        public DateTime DateClosed { get; set; }
+        [Required]
+        public StatusEnum Status { get; set; }
+        public IdentityUser Client { get; set; }
+        [Required]
+        public string ClientId { get; set; }
+        public IdentityUser Employee { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
